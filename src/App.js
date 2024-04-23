92dd58f37670a1ecd4f18834a9ab89f1c9639b13
merve <merveyldrm9497@gmail.com>
2024-04-23 12:29:32 +0300

    useMovies: Use custom hook.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating"
+import { useMovies } from "./useMovies";
 
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -8,9 +9,6 @@ const KEY = "b4feddf8";
 
 export default function App() {
 	const [query, setQuery] = useState("");
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
 	const [selectedId, setSelectedId] = useState(null);
 
 	const [watched, setWatched] = useState(function () {
@@ -38,47 +36,7 @@ export default function App() {
 		localStorage.setItem("watched", JSON.stringify(watched));
 	}, [watched]);
 
-	useEffect(function () {
-		const controller = new AbortController();
-		async function fetchMovies() {
-			try {
-				setIsLoading(true);
-				setError("") // reset error
-				const res = await fetch(
-					`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-					{ signal: controller.signal }
-				);
-
-				if (!res.ok) throw new Error("Somthing went wrong with fetching movies");
-
-				const data = await res.json();
-				if (data.Response === "False") throw new Error("Movie not found");
-
-				setMovies(data.Search);
-				setSelectedId(data?.Search[0]?.imdbID)
-				setError("");
-			} catch (err) {
-				if (err.name !== "AbortError") {
-					console.log(err.message);
-					setError(err.message);
-				}
-			} finally {
-				setIsLoading(false);
-			}
-		}
-
-		if (query.length < 3) {
-			setMovies([]);
-			setError("") // reset error
-			return;
-		}
-		handleCloseMovie();
-		fetchMovies();
-
-		return () => {
-			controller.abort(); // cleanup controller
-		}
-	}, [query]);
+	const { movies, isLoading, error } = useMovies(query);
 
 	return (
 		<>
